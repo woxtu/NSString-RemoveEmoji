cf3c388733fffcfb80190698b06125723317832e
Mark Rogers <mark.rogers@yodle.com>
2015-07-20 11:01:58 -0400

    Added tests for new surrogate pair functionality.

--- NSString+RemoveEmojiTests/NSString_RemoveEmojiTests.m
+++ NSString+RemoveEmojiTests/NSString_RemoveEmojiTests.m
@@ -9,15 +9,27 @@
 @implementation NSString_RemoveEmojiTests
 
 - (void)testHasEmoji {
-    XCTAssertFalse([@"Party Hard" isIncludingEmoji]);
-    XCTAssert([@"✨" isIncludingEmoji]);
-    XCTAssert([@"✨Party✨ ✨Hard✨" isIncludingEmoji]);
+	XCTAssertFalse([@"Party Hard" isIncludingEmoji]);
+	XCTAssert([@"✨" isIncludingEmoji]);
+	XCTAssert([@"✨Party✨ ✨Hard✨" isIncludingEmoji]);
 }
 
 - (void)testRemovedEmojiString {
-    XCTAssert([[@"Party Hard" removedEmojiString] isEqualToString:@"Party Hard"]);
-    XCTAssert([[@"✨" removedEmojiString] isEqualToString:@""]);
-    XCTAssert([[@"✨Party✨ ✨Hard✨" removedEmojiString] isEqualToString:@"Party Hard"]);
+	XCTAssert([[@"Party Hard" removedEmojiString] isEqualToString:@"Party Hard"]);
+	XCTAssert([[@"✨" removedEmojiString] isEqualToString:@""]);
+	XCTAssert([[@"✨Party✨ ✨Hard✨" removedEmojiString] isEqualToString:@"Party Hard"]);
+}
+
+- (void)testHasSurrogate {
+	XCTAssertFalse([@"Business Professional ☺️" isIncludingSurrogatePair]);
+	XCTAssert([@"🌍" isIncludingSurrogatePair]);
+	XCTAssert([@"Business🌍 Professional ☺️" isIncludingSurrogatePair]);
+}
+
+- (void)testRemovedSurrogateString {
+	XCTAssert([[@"Business Professional ☺️" removedSurrogatePairString] isEqualToString:@"Business Professional ☺️"]);
+	XCTAssert([[@"🌍" removedSurrogatePairString] isEqualToString:@""]);
+	XCTAssert([[@"Business🌍 Professional ☺️" removedSurrogatePairString] isEqualToString:@"Business Professional ☺️"]);
 }
 
 @end
