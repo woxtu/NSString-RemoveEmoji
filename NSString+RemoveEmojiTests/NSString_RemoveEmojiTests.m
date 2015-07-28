6872d2d6078c9d183c0ab3eab7fe2768509c686f
Mark Rogers <Mark@Marks-MacBook-Pro.local>
2015-07-28 10:27:35 -0400

    Reverted original files.

--- NSString+RemoveEmojiTests/NSString_RemoveEmojiTests.m
+++ NSString+RemoveEmojiTests/NSString_RemoveEmojiTests.m
@@ -9,15 +9,15 @@
 @implementation NSString_RemoveEmojiTests
 
 - (void)testHasEmoji {
-	XCTAssertFalse([@"Party Hard" isIncludingEmoji]);
-	XCTAssert([@"✨" isIncludingEmoji]);
-	XCTAssert([@"✨Party✨ ✨Hard✨" isIncludingEmoji]);
+    XCTAssertFalse([@"Party Hard" isIncludingEmoji]);
+    XCTAssert([@"✨" isIncludingEmoji]);
+    XCTAssert([@"✨Party✨ ✨Hard✨" isIncludingEmoji]);
 }
 
 - (void)testRemovedEmojiString {
-	XCTAssert([[@"Party Hard" removedEmojiString] isEqualToString:@"Party Hard"]);
-	XCTAssert([[@"✨" removedEmojiString] isEqualToString:@""]);
-	XCTAssert([[@"✨Party✨ ✨Hard✨" removedEmojiString] isEqualToString:@"Party Hard"]);
+    XCTAssert([[@"Party Hard" removedEmojiString] isEqualToString:@"Party Hard"]);
+    XCTAssert([[@"✨" removedEmojiString] isEqualToString:@""]);
+    XCTAssert([[@"✨Party✨ ✨Hard✨" removedEmojiString] isEqualToString:@"Party Hard"]);
 }
 
 @end
