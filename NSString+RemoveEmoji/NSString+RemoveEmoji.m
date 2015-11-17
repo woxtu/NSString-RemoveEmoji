e15f956c471f90450914fa4c5953a62281ec9c46
woxtu <woxtup@gmail.com>
2015-11-17 21:59:48 +0900

    Fix a wrong comment

--- NSString+RemoveEmoji/NSString+RemoveEmoji.m
+++ NSString+RemoveEmoji/NSString+RemoveEmoji.m
@@ -4,14 +4,14 @@
 
 - (BOOL)isEmoji {
     const unichar high = [self characterAtIndex: 0];
-    
-    // Surrogate pair (U+1D000-1F77F)
+
+    // Surrogate pair (U+1D000-1F9FF)
     if (0xD800 <= high && high <= 0xDBFF) {
         const unichar low = [self characterAtIndex: 1];
         const int codepoint = ((high - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
-        
+
         return (0x1D000 <= codepoint && codepoint <= 0x1F9FF);
-        
+
     // Not surrogate pair (U+2100-27BF)
     } else {
         return (0x2100 <= high && high <= 0x27BF);
@@ -20,7 +20,7 @@
 
 - (BOOL)isIncludingEmoji {
     BOOL __block result = NO;
-    
+
     [self enumerateSubstringsInRange:NSMakeRange(0, [self length])
                              options:NSStringEnumerationByComposedCharacterSequences
                           usingBlock: ^(NSString* substring, NSRange substringRange, NSRange enclosingRange, BOOL* stop) {
@@ -29,19 +29,19 @@
             result = YES;
         }
     }];
-    
+
     return result;
 }
 
 - (instancetype)stringByRemovingEmoji {
     NSMutableString* __block buffer = [NSMutableString stringWithCapacity:[self length]];
-    
+
     [self enumerateSubstringsInRange:NSMakeRange(0, [self length])
                              options:NSStringEnumerationByComposedCharacterSequences
                           usingBlock: ^(NSString* substring, NSRange substringRange, NSRange enclosingRange, BOOL* stop) {
         [buffer appendString:([substring isEmoji])? @"": substring];
     }];
-    
+
     return buffer;
 }
 
