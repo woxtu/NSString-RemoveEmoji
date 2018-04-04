348517737d6d38337be8c2d8e10c2f234745ec76
Alessandro Martin <martin.acg@gmail.com>
2018-04-04 20:41:08 +0100

    Exposed containsEmoji() to Objective C

--- Sources/NSString+RemoveEmoji.swift
+++ Sources/NSString+RemoveEmoji.swift
@@ -8,17 +8,20 @@
 
 import Foundation
 
-fileprivate let CombiningEnclosingKeycap: UInt32 = 0x20E3
-fileprivate let VaridationSelector15: UInt32 = 0xFE0E
-fileprivate let VaridationSelector16: UInt32 = 0xFE0F
+private extension UInt32 {
+    static let combiningEnclosingKeycap: UInt32 = 0x20E3
+    static let varidationSelector15: UInt32 = 0xFE0E
+    static let varidationSelector16: UInt32 = 0xFE0F
+}
 
 public extension NSString {
+    @objc
     public func containsEmoji() -> Bool {
         let codepoints = (self as String).unicodeScalars.map { $0.value }
         
         if let first = codepoints.first, let last = codepoints.last {
-            let isKeycapEmoji = last == CombiningEnclosingKeycap && codepoints.contains(VaridationSelector16)
-            let isEmoji = CodePointSet.contains(first) && last != VaridationSelector15
+            let isKeycapEmoji = last == .combiningEnclosingKeycap && codepoints.contains(.varidationSelector16)
+            let isEmoji = CodePointSet.contains(first) && last != .varidationSelector15
             
             return isKeycapEmoji || isEmoji
         } else {
